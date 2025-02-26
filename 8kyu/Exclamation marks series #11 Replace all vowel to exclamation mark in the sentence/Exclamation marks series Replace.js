5aeaa84c5b6be52e7db36696ae44944b6ebb5be4
Fergus-son <sarapovnikolaj790@gmail.com>
2025-02-27 01:21:40 +0300

    Create Exclamation marks series Replace.js

---
+++ 8kyu/Exclamation marks series #11 Replace all vowel to exclamation mark in the sentence/Exclamation marks series Replace.js
@@ -0,0 +1,30 @@
+function replace(s) {
+    //coding and coding....
+    
+      return s.replace(/[aeiouAEIOU]/g, '!');
+  }
+  
+  console.log("Hi!")
+  console.log("!Hi! Hi!")
+  console.log("aeiou")
+  console.log("ABCDE")
+
+  
+// Sample Test
+
+// describe("Basic Tests", function () {
+//   it("It should work for sample tests", function () {
+//       doTest("Hi!", "H!!");
+//       doTest("!Hi! Hi!", "!H!! H!!");
+//       doTest("aeiou", "!!!!!");
+//       doTest("ABCDE", "!BCD!");
+//   });
+
+//   const { assert } = require('chai');
+
+//   function doTest(input, expected) {
+//       const message = `for s = ${JSON.stringify(input)}\n`;
+//       const actual = replace(input);
+//       assert.strictEqual(actual, expected, message);
+//   }
+// });
